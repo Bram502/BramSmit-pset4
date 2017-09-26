9a206858e00f460402b8f8336577574d26950bb6
Bram <bwhsmit2@hotmail.com>
2017-09-26 13:41:43 +0200

    dialog version

--- app/src/main/java/com/example/bwhsm/bramsmit_pset4/MainActivity.java
+++ app/src/main/java/com/example/bwhsm/bramsmit_pset4/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bwhsm.bramsmit_pset4;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,7 +13,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText inputText;
+    String inputText;
     TextView txtView;
     DBHandler dbHandler;
 
@@ -18,37 +21,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inputText = (EditText) findViewById(R.id.textInput);
         txtView = (TextView) findViewById(R.id.textView);
         dbHandler = new DBHandler(this,null,null,1);
 
         findViewById(R.id.addButton).setOnClickListener(new addButtonClicked());
-        findViewById(R.id.deleteButton).setOnClickListener(new deleteButtonClicked());
+//        findViewById(R.id.deleteButton).setOnClickListener(new deleteButtonClicked());
     }
 
     public void printDatabase(){
         String dbString = dbHandler.databaseToString();
         txtView.setText(dbString);
-        inputText.setText("");
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Task");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                inputText = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
     private class addButtonClicked implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            Item item = new Item(inputText.getText().toString());
+            showDialog();
+            Item item = new Item(inputText);
             dbHandler.addItem(item);
             printDatabase();
         }
     }
 
-
-    private class deleteButtonClicked implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            dbHandler.deleteItem(inputText.getText().toString());
-            printDatabase();
-        }
-    }
+//
+//    private class deleteButtonClicked implements View.OnClickListener {
+//        @Override
+//        public void onClick(View view) {
+//            dbHandler.deleteItem(inputText.getText().toString());
+//            printDatabase();
+//        }
+//    }
 
 
 
